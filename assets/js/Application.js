6569247961ccc585718c3c51d04fcb8f545ac994
jerome Laroy <laroy.jerome@gmail.com>
2019-06-09 12:35:42 +0200

    Upload v 0.2.0

--- assets/js/Application.js
+++ assets/js/Application.js
@@ -1,5 +1,5 @@
+import Canvas from './Library/Canvas.js'
 import Tests from './Tests/Tests.js'
-import Vector2 from './Library/Math/Vector2.js'
 
 /** Class for the application. */
 export default class Application {
@@ -7,29 +7,36 @@ export default class Application {
      * Create a new application.
      */
     constructor() {
-        const tests = true
+        const tests = false
         if (tests) {
             new Tests()
         }
         console.info('WebGL2 Demo')
 
-        const v1 = new Vector2
-        const v2 = new Vector2(1.5, 2.5)
-        console.log('start:', v1, v2)
-        v1.sub(v2)
-        console.log('v1 - v2:', v1, v2)
-        const a = 10
-        v1.scalar(a)
-        console.log('a * v1:', a, v1)
-        console.log('norm of v1:', v1.norm())
-        const v3 = new Vector2(3, 5)
-        const v4 = new Vector2(5, 3)
-        const v5 = new Vector2(-3, -5)
-        console.log('v3 . v4:', v3.dot(v4))
-        console.log('v4 . v5:', v4.dot(v5))
-        const v6 = new Vector2(3, 1)
-        console.log('v6', v6)
-        v6.rot(90)
-        console.log('v6.rot(90)', v6)
+        this.shaderSources = {
+            fragment: null,
+            vertex: null,
+        }
+        this.preloader()
+    }
+
+    async preloader() {
+        console.info('Preloading source code for shaders')
+        await fetch('./assets/glsl/vertex-shader.glsl')
+            .then(response => response.text())
+            .then(source => this.shaderSources.vertex = source)
+            .catch(error => console.error(error.message))
+        await fetch('./assets/glsl/fragment-shader.glsl')
+            .then(response => response.text())
+            .then(source => this.shaderSources.fragment = source)
+            .catch(error => console.error(error.message))
+        this.run()
+    }
+
+    run() {
+        const width = 600
+        const height = 480
+
+        new Canvas(width, height, this.shaderSources)
     }
 }
